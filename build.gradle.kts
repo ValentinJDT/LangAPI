6da64e708e7567419a3f5455efb1e3593eb6cd97
ValentinJDT <vjeandot@yahoo.com>
2023-05-29 22:23:11 +0200

    add publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    `maven-publish`
 }
 
 group = "fr.mrcoq.lib"
@@ -15,4 +16,22 @@ repositories {
 
 dependencies {
     compileOnly("org.spigotmc:spigot-api:1.19.4-R0.1-SNAPSHOT")
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/ValentinJDT/MultiLanguageAPI\n")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
 }
