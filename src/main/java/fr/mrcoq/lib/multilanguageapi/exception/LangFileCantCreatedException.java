1a9398339353fea799a82aefd4cdd81e5aa20b27
ValentinJDT <vjeandot@yahoo.com>
2023-06-01 23:36:15 +0200

    remove empty line

--- src/main/java/fr/mrcoq/lib/multilanguageapi/exception/LangFileCantCreatedException.java
+++ src/main/java/fr/mrcoq/lib/multilanguageapi/exception/LangFileCantCreatedException.java
@@ -1,7 +1,6 @@
 package fr.mrcoq.lib.multilanguageapi.exception;
 
 public class LangFileCantCreatedException extends Exception {
-
     public LangFileCantCreatedException() {
         super("Can't create lang file configuration.");
     }
